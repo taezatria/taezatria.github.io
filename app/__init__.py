ab8854edee16f2df68def975c6f9f096c7bccb35
Satria Damai Kurnia Hu <m26415172@john.petra.ac.id>
2017-03-25 22:59:41 +0700

    coba

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_frozen import Freezer
+from flask_flatpages import FlatPages
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config.from_pyfile('settings.py')
 freezer = Freezer(app)
+pages = FlatPages(app)
