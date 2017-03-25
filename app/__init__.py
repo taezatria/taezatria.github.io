03032052472b45a27766b48a87cfb39e3c275dc3
Satria Damai Kurnia Hu <m26415172@john.petra.ac.id>
2017-03-25 23:15:32 +0700

    initialize

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_frozen import Freezer
-from flask_flatpages import FlatPages
-
-app = Flask(__name__)
-app.config.from_object('config')
-app.config.from_pyfile('settings.py')
-freezer = Freezer(app)
-pages = FlatPages(app)
--- freeze.py
+++
@@ -1,4 +0,0 @@
-from app import freezer
-
-if __name__ == '__main__':
-	freezer.freeze()
